dd5446ba59fb8f051902fbc4b758088d809554d6
Dominic-Bartlomowicz <dbartlomowicz@gmail.com>
2018-05-08 03:04:59 -0700

    Pushed to Github

--- back-end/routes/routes.js
+++ back-end/routes/routes.js
@@ -8,8 +8,20 @@ const appRouter = app => {
     res.status(200).sendFile("index.html");
   });
 
+  app.get("/users/:id/albums", (req, res) => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${req.params.id}/albums`)
+      .then(response => response.json())
+      .then(data => res.status(200).send(data));
+  });
+
+  app.get("/albums/:id/photos", (req, res) => {
+    fetch(`https://jsonplaceholder.typicode.com/albums/${req.params.id}/photos`)
+      .then(response => response.json())
+      .then(data => res.status(200).send(data));
+  });
+
   app.get("/users", (req, res) => {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch(`https://jsonplaceholder.typicode.com/users/`)
       .then(response => response.json())
       .then(data => res.status(200).send(data));
   });
--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,24 +1,71 @@
 const URL = "http://localhost:3000";
 
 $(document).ready(() => {
+
   $.get(`${URL}/users`, data => {
     buildUserList(data);
   });
+
+
+  const buildUserList = userData => {
+    userData.forEach(user => {
+      const { id, name } = user;
+      const html = `<a href="#!" user-id=${id} class="collection-item user-list-item">${name}</a>`;
+      $("#user-list").append(html);
+    });
+
+    $(".user-list-item").click(function() {
+      const userId = $(this).attr("user-id");
+      onSelectUser(userId);
+    });
+  };
+
+
+  const buildAlbumList = albumData => {
+    $("#photo-list").empty();
+    $("#album-list").empty();
+    albumData.forEach(album => {
+      const { id, title } = album;
+      const html = `<a href="#!" album-id=${id} class="collection-item album-list-item">${title}</a>`;
+      $("#album-list").append(html);
+    });
+
+    $(".album-list-item").click(function() {
+      const albumId = $(this).attr("album-id");
+      onSelectAlbum(albumId);
+    });
+  }
+
+  const buildPhotoList = photoData => {
+    $("#photo-list").empty();
+    $("#album-list").empty();
+    photoData.forEach(photo => {
+      const { id, title, thumbnailUrl } = photo;
+
+      const image = `<img class="image" src="${thumbnailUrl}">`
+      $("#user-photo").append(image);
+
+      // const html = `<a photo-id=${id} class="collection-item photo-list-item">${title}</a>`;
+      // $("#photo-list").append(html);
+    });
+  };
+
+
+  const onSelectUser = userId => {
+    console.log(`Now get user ${userId}'s albums...`);
+
+    $.get(`${URL}/users/${userId}/albums`, data => {
+      buildAlbumList(data);
+    });
+  };
+
+
+  const onSelectAlbum = albumId => {
+    console.log(`Now get user ${albumId}'s photos...`);
+
+    $.get(`${URL}/albums/${albumId}/photos`, data => {
+      buildPhotoList(data);
+    });
+  };
+
 });
-
-const buildUserList = userData => {
-  userData.forEach(user => {
-    const { id, name } = user;
-    const html = `<a href="#!" user-id=${id} class="collection-item user-list-item">${name}</a>`;
-    $("#user-list").append(html);
-  });
-
-  $(".user-list-item").click(function() {
-    const userId = $(this).attr("user-id");
-    onSelectUser(userId);
-  });
-};
-
-const onSelectUser = userId => {
-  console.log(`Now get user ${userId}'s albums...`);
-};
